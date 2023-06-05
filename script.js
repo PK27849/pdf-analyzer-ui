d061edde4b0bcd41ab2810c4b4af0b49cdcdd241
PK27849 <124418438+PK27849@users.noreply.github.com>
2023-06-05 11:34:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,59 +6,63 @@ function showFileName(input) {
   }
 
   async function run(event) {
-    event.preventDefault();
-  
-    var fileInput = document.getElementById('file');
-    var file = fileInput.files[0];
-  
-    var openAIInput = document.getElementById('OpenAI');
-    var openAIKey = openAIInput.value;
-  
-    var questionInput = document.getElementById('question');
-    var question = questionInput.value;
-  
-    var spinnerContainer = document.getElementById('spinner');
-    var responseText = document.getElementById('response-text');
-    responseText.value = '';
-  
-    // Validate required fields
-    if (!file || !openAIKey || !question) {
-      // responseText.value = 'Please fill in all the required fields.';
-      showAlert();
-      return;
-    }
-  
-    var apiUrl = 'https://prasank02-pdf-search-api.hf.space/search';
-    var formData = new FormData();
-    formData.append('file', file);
-    formData.append('api_key', openAIKey);
-    formData.append('text', question);
-  
-    spinnerContainer.style.display = 'flex'; // Show the spinner before making the API request
-    document.body.classList.remove('blur'); // Remove blur from the background
-    try {
-      // document.body.classList.add('blur'); // Apply blur to the background
-  
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        body: formData
-        timeout: 120000 // Set the timeout to 60 seconds (60000 milliseconds)
-      });
-  
-      if (!response.ok) {
-        throw new Error('Error occurred while fetching the response.');
-      }
-  
-      const data = await response.json();
-      responseText.value = data.response;
-    } catch (error) {
-      console.error('Error:', error);
-      responseText.value = 'Error occurred while fetching the response.';
-    } finally {
-      spinnerContainer.style.display = 'none'; // Hide the spinner after receiving the response or error
-      document.body.classList.remove('blur'); // Remove blur from the background
-    }
+  event.preventDefault();
+
+  var fileInput = document.getElementById('file');
+  var file = fileInput.files[0];
+
+  var openAIInput = document.getElementById('OpenAI');
+  var openAIKey = openAIInput.value;
+
+  var questionInput = document.getElementById('question');
+  var question = questionInput.value;
+
+  var spinnerContainer = document.getElementById('spinner');
+  var responseText = document.getElementById('response-text');
+  responseText.value = '';
+
+  // Validate required fields
+  if (!file || !openAIKey || !question) {
+    showAlert();
+    return;
   }
+
+  var apiUrl = 'https://prasank02-pdf-search-api.hf.space/search';
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('api_key', openAIKey);
+  formData.append('text', question);
+
+  spinnerContainer.style.display = 'flex'; // Show the spinner before making the API request
+  document.body.classList.remove('blur'); // Remove blur from the background
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      body: formData,
+      timeout: 120000 // Set the timeout to 60 seconds (60000 milliseconds)
+    });
+
+    if (!response.ok) {
+      throw new Error('Error occurred while fetching the response.');
+    }
+
+    const data = await response.json();
+    responseText.value = data.response;
+  } catch (error) {
+    console.error('Error:', error);
+    if (error.name === 'AbortError') {
+      // Timeout error occurred
+      responseText.value = 'Timeout error occurred while fetching the response.';
+    } else {
+      responseText.value = 'Error occurred while fetching the response.';
+    }
+  } finally {
+    spinnerContainer.style.display = 'none'; // Hide the spinner after receiving the response or error
+    document.body.classList.remove('blur'); // Remove blur from the background
+  }
+}
+
 
   function showAlert() {
     swal("Error!", "Please fill all the details", "error");
